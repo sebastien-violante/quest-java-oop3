adb11cba60910f5d3182dfbe6df3e57ef755dca0
sebastien-violante <sebastien.violante@gmail.com>
2023-08-29 11:52:55 +0200

    end of quest

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,64 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+    
+    /**
+     * to take off, the eagle must be on ground
+     */
+    @Override
+    public void takeOff() {
+        if (!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.printf("%s takes off in the sky.%n", this.getName());
+        }
+    }
+    
+    /**
+     * ascend
+     * @param meters altitude increase
+     * @return altitude
+     */
+    @Override
+    public int ascend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude + meters, 400);
+            System.out.printf("%s flies upward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+    /**
+     * descend
+     * @param meters altitude decrease
+     * @return altitude
+     */
+    @Override
+    public int descend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude - meters, 0);
+            System.out.printf("%s flies downward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+    
+    /**
+     * glide
+     */
+    @Override
+    public void glide() {
+        System.out.printf("glides into the air.%n");
+    }
+    
+    /**
+     * to land, the bird must be at one meter high maximum
+     */
+    @Override
+    public void land() {
+        if (this.flying && this.altitude <= 1) {
+            System.out.printf("%s lands on the ground.%n", this.getName());
+        } else {
+            System.out.printf("%s is too high, it can't land.%n", this.getName());
+        }
+    }
+     
 }
